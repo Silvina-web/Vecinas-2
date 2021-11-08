01d2d3079a1725bd50c7932500b3b006efd10b1d
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2021-11-08 13:43:56 -0300

    Validacion y js

--- main.js
+++ main.js
@@ -2,6 +2,45 @@ $(document).ready(function(){
   $('#login').hide();
 
   $("#desplegable").click(function(){
-      $("#login").slideToggle();
+      $("#login").slideToggle(1500);
   })
 })
+
+
+let intentos = 1;
+function validar(){
+//Variables
+  let usuario = $("#nombre").val();
+  let contraseña = $("#pass").val();
+  let mail =$("#mail").val();
+
+//intentos
+  if(intentos <=3){
+       if(usuario == "Silvina" && contraseña == "123" && mail == "silvina.marcantoni@gmail.com"){
+         swal ("Validacion",
+               "Bienvenida",
+                "success");
+                intentos=1;
+       }else{
+        swal ("Validacion",
+               "No no",
+              "error");
+                intentos ++; 
+
+       }
+
+       }else{
+         swal("validacion",
+               "Usuario bloquedo",
+               "error");
+       }
+  }
+
+
+
+
+
+
+
+
+  
