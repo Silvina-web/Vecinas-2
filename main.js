a184adabcbd0466dda08549a53dfdb9818d8a397
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2021-11-16 19:05:57 -0300

    Cotizador

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Login
+
 $(document).ready(function(){
   $('#login').hide();
 
@@ -38,9 +40,132 @@ function validar(){
 
 
 
+//PREVENT DEFAULT
+  let submitBtn= document.getElementById("enviar");
+  submitBtn.addEventListener('click', (e)=>{
+     e.preventDefault();
+    console.log('Boton clickeado');
+    console.log (e.target);
+  }
+  )
 
 
 
+//---CARD  CONTEINER ---id= "card-conteiner"---------------
+let serviciosJSON =[];
+let carro = [];
+
+const mainConteinerCards = document.getElementById("card-conteiner");
+console.log(mainConteinerCards);
+
+//json
+const obtenerJson = () => {
+  const miJson = "servicios.json";
+  $.getJSON(miJson, function(respuesta, estado){
+    if(estado == "success"){
+      serviciosJSON= respuesta;
+      console.log(serviciosJSON);
+      pintarServicios();
+    }
+  });
+}
 
 
+obtenerJson();
+
+function pintarServicios(){
+    for( const servicio of serviciosJSON){
+  let conteinerCards = document.createElement ("div");
+  conteinerCards.innerHTML = `<h5> id: ${servicio.id}</h5>
+                              <h5>  ${servicio.nombre}</h5>
+                              <h2> $ ${servicio.precio}</h2>
+                               <button id= "${servicio.id}"> Adquirir</button> `;
+
+mainConteinerCards.appendChild(conteinerCards);
+//evento boton adquirir
+document.getElementById(`${servicio.id}`).onclick =()=> agregarAlCarrito(`${servicio.id}`);
+
+}
+}
+function agregarAlCarrito(id){
+  carro.push(serviciosJSON[id-1]);
+  console.log(carro);
+     $("#resumen").append(`
+                     <tr>
+                      <td>${(serviciosJSON[id-1]).id}</td>
+                      <td>${(serviciosJSON[id-1].nombre)}</td>
+                      <td><input type ="number"  value= 1 id="multiplicador"></input></td>
+                      <td>${(serviciosJSON[id-1].precio)}</td>
+                      <td><button  id="delete" class="btn btn-danger ">x</button></td>
+                      </tr>`)
+
+ document.getElementById("delete").addEventListener('click', (e)=> e.target.parentElement.parentElement.remove());
+ document.getElementById("multiplicador").addEventListener('change', cambiarCantidad);
+ totalCarro();
+
+
+localStorage.setItem("Carro",JSON.stringify(carro));
+}
+
+
+//Cantidad no menos a 1
+function cambiarCantidad (event){
+ const input=  event.target;
+ if(input.value <=0){
+   input.value=1;
+ }
+}
+
+function totalCarro(){
+  let total = 0;
+  for( const serv of carro){
+    total += serv.precio;
+    console.log(total);
   
+
+  }
+precioUnitario.innerHTML= total;
+totalServicios.innerHTML= carro.length;
+
+}
+
+const tableConteiner = document.getElementById("table");  
+
+let precioTotal = document.createElement("h2");
+precioTotal.innerHTML = "El total de su compra es $";
+tableConteiner.appendChild(precioTotal);
+
+let precioUnitario = document.createElement("h3");
+precioUnitario.innerHTML =  "0";
+tableConteiner.appendChild(precioUnitario);
+
+let cantidadServicios = document.createElement("h5");
+cantidadServicios.innerHTML ="Total servicios: ";
+tableConteiner.appendChild(cantidadServicios);
+
+let totalServicios = document.createElement("h2");
+totalServicios.innerHTML = "0";
+tableConteiner.appendChild(totalServicios);
+
+let reiniciar = document.createElement("button");
+reiniciar.innerHTML = "Recalculando";
+reiniciar.setAttribute("id","reiniciar");
+tableConteiner.appendChild(reiniciar);
+
+reiniciar.onclick =()=>{
+  carro = [];
+  precioUnitario.innerHTML= "0";
+  totalServicios.innerHTML= "No ha contratado nada";
+  
+  
+  localStorage.removeItem("carro");
+
+}
+console.log(carro);
+
+
+
+
+
+//API DOLAR
+//URL: https://api-dolar-argentina.herokuapp.com/
---
+++ servicios.js
@@ -0,0 +1,23 @@
+const servicios = [
+    { id: 1, nombre:"Pagina Web", precio: 520 },
+    { id: 2, nombre: "One Page", precio: 500  },
+    { id: 3, nombre:"Logo", precio: 250},
+    { id: 4, nombre:"Google Analitics", precio: 350},
+    { id: 5, nombre: "Manejo de redes", precio: 850},
+    {  id: 6, nombre:"Agregado de fotos", precio: 150},
+    {  id: 7, nombre:"Agregado de sessiones", precio: 180}
+]
+console.log(servicios);
+
+
+
+
+
+
+
+
+
+
+
+
+  
