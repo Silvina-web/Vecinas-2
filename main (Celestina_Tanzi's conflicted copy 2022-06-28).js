133f2a462e1afb00099114865ac7a7214bdd772d
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2022-07-12 21:06:45 -0300

    arreglos

---
+++ main (Celestina_Tanzi's conflicted copy 2022-06-28).js
@@ -0,0 +1,240 @@
+///////CARD  CONTEINER ---id= "card-conteiner"///////////////////////////////////////
+
+let serviciosJSON =[];
+let carro = [];
+
+const mainConteinerCards = document.getElementById("card-conteiner");
+console.log(mainConteinerCards);
+
+//json
+const obtenerJson = () => {
+  const miJson = "servicios.json";
+  $.getJSON(miJson, function(respuesta, estado){
+    if(estado == "success"){
+      serviciosJSON= respuesta;
+      console.log(serviciosJSON);
+      pintarServicios();
+    }
+  });
+}
+
+
+obtenerJson();
+// Renderizo cards 
+function pintarServicios(){
+    for( const servicio of serviciosJSON){
+  let conteinerCards = document.createElement ("div");
+  conteinerCards.innerHTML = `<h5 style="display: none"> id: ${servicio.id}</h5>
+                              <h5>  ${servicio.nombre}</h5>
+                              <p> ${servicio.item}</p>
+                              <h2> $ ${servicio.precio}</h2>
+                              <button id="${servicio.id}"> Adquirir</button> `;
+
+mainConteinerCards.appendChild(conteinerCards);
+
+//evento boton adquirir con js
+document.getElementById(`${servicio.id}`).onclick =()=> agregarAlCarrito(`${servicio.id}`);
+
+    }
+}
+
+//Tabla renderizada con JQuery
+function agregarAlCarrito(id){
+   carro.push(serviciosJSON[id-1]);
+   console.log(carro);
+     $("#resumen").append(`
+                      <tr class="fila">
+                      <td>${(serviciosJSON[id-1]).id}</td>
+                      <td>${(serviciosJSON[id-1].nombre)}</td>
+                      <td  id="precio">${(serviciosJSON[id-1].precio)}</td>
+                      
+            
+                      </tr>`)
+
+ 
+addEvent_borrar();
+totalCarro();
+
+//Lo agrego al Local Storage
+ localStorage.setItem("Carro",JSON.stringify(carro));
+}
+
+//Borro cada item de la tabla
+
+function addEvent_borrar() {
+
+  let btnDelete = document.querySelectorAll('.delete');
+
+  console.log(btnDelete);
+
+  btnDelete.forEach(element => {
+
+      element.addEventListener('click', borraLinea);
+
+             function borraLinea() {
+
+             element.parentNode.parentNode.remove();
+            
+           for(const item of carro){
+            if(
+              element.closest("td").previousElementSibling
+              .previousElementSibling.textContent == item.nombre)
+              {
+              let indice= carro.indexOf(item);
+              carro.splice(indice, 1);
+              console.log(indice);
+
+              
+
+              totalCarro();
+              
+            }
+          
+          }
+          
+ }
+
+ });
+
+}
+  
+
+
+  
+      
+
+             
+
+             
+          
+          
+           
+// Cantidad de dinero y servicios contratados
+
+
+function totalCarro(){
+ let total = 0;
+  for( const serv of carro){
+        total += serv.precio;
+        console.log(total);
+  }
+        precioUnitario.innerHTML = total;
+        totalServicios.innerHTML = carro.length;
+        
+  }
+
+    
+
+
+
+const tableConteiner = document.getElementById("resumen-total");  
+
+let precioTotal = document.createElement("h2");
+precioTotal.innerHTML = "El total de su compra es $";
+tableConteiner.appendChild(precioTotal);
+
+let precioUnitario = document.createElement("h3");
+precioUnitario.innerHTML =  "0";
+tableConteiner.appendChild(precioUnitario);
+
+let cantidadServicios = document.createElement("h5");
+cantidadServicios.innerHTML ="Total servicios: ";
+tableConteiner.appendChild(cantidadServicios);
+
+let totalServicios = document.createElement("h2");
+totalServicios.innerHTML = "0";
+tableConteiner.appendChild(totalServicios);
+
+
+let reiniciar = document.createElement("button");
+reiniciar.innerHTML = "Recalculando";
+reiniciar.setAttribute("id","reiniciar");
+tableConteiner.appendChild(reiniciar);
+
+
+//Boton recalculando
+reiniciar.onclick =()=>{
+  carro = [];
+  precioUnitario.innerHTML= "0";
+  totalServicios.innerHTML= "No ha contratado nada";
+  $('.fila').remove();
+  
+
+  console.log(carro);
+
+  //localStorage.removeItem("carro");
+
+}
+
+
+
+
+
+//Formulario Footer validacion
+//Funcion llamada desde linea 210 HTML Form
+function validacion(){
+ var  name,email,telefono,expresion;
+  name = document.getElementById("name").value;
+  email=document.getElementById("email").value;
+  telefono=document.getElementById("telefono").value;
+  expresion= /\w+@\w+\.+[a-z]/;
+
+  if (name==="" || email ==="" || telefono===""){
+    alert("Todos los campos son obligatorios");
+    return false;
+
+  }
+  else if (name.length > 25){
+    alert("Nombre largo");
+    return false;
+  }
+  else if (email.length > 30){
+    alert("Mail incorrecto");
+    return false;
+  } else if(!expresion.test(email)){
+    alert("Mail no cumple");
+    return false;
+
+  }
+  else if (isNaN(telefono)){
+    alert("Telefono invalido");
+    return false;
+  } else if (telefono.length > 20){
+    alert("Telefono largo");
+  }
+  else{
+    
+    const afirmativo=prompt("Genial!!Desea guardar los datos?");
+    var name=name;
+     if(afirmativo ==="si"){
+       localStorage.setItem("nombre",name)
+       localStorage.getItem(name)
+       alert("Hola" + " "+ name+ " "+ "gracias por mirar nuestros servicios!")
+      
+     }else{
+       sessionStorage.removeItem(name)
+
+     }
+
+  }
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//API DOLAR
+//URL: https://api-dolar-argentina.herokuapp.com/
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 ///////CARD  CONTEINER ---id= "card-conteiner"///////////////////////////////////////
 
-let serviciosJSON =[];
+let serviciosJSON = [];
 let carro = [];
 
 const mainConteinerCards = document.getElementById("card-conteiner");
@@ -8,41 +8,42 @@ console.log(mainConteinerCards);
 
 //json
 const obtenerJson = () => {
-  const miJson = "servicios.json";
-  $.getJSON(miJson, function(respuesta, estado){
-    if(estado == "success"){
-      serviciosJSON= respuesta;
-      console.log(serviciosJSON);
-      pintarServicios();
-    }
-  });
+    const miJson = "servicios.json";
+    $.getJSON(miJson, function(respuesta, estado) {
+        if (estado == "success") {
+            serviciosJSON = respuesta;
+            console.log(serviciosJSON);
+            pintarServicios();
+        }
+    });
 }
 
 
 obtenerJson();
+
+
 // Renderizo cards 
-function pintarServicios(){
-    for( const servicio of serviciosJSON){
-  let conteinerCards = document.createElement ("div");
-  conteinerCards.innerHTML = `<h5 style="display: none"> id: ${servicio.id}</h5>
+function pintarServicios() {
+    for (const servicio of serviciosJSON) {
+        let conteinerCards = document.createElement("div");
+        conteinerCards.innerHTML = `<h5 style="display: none"> id: ${servicio.id}</h5>
                               <h5>  ${servicio.nombre}</h5>
-                              <p> ${servicio.item}</p>
                               <h2> $ ${servicio.precio}</h2>
                               <button id="${servicio.id}"> Adquirir</button> `;
 
-mainConteinerCards.appendChild(conteinerCards);
+        mainConteinerCards.appendChild(conteinerCards);
 
-//evento boton adquirir con js
-document.getElementById(`${servicio.id}`).onclick =()=> agregarAlCarrito(`${servicio.id}`);
+        //evento boton adquirir con js
+        document.getElementById(`${servicio.id}`).onclick = () => agregarAlCarrito(`${servicio.id}`);
 
     }
 }
 
 //Tabla renderizada con JQuery
-function agregarAlCarrito(id){
-   carro.push(serviciosJSON[id-1]);
-   console.log(carro);
-     $("#resumen").append(`
+function agregarAlCarrito(id) {
+    carro.push(serviciosJSON[id - 1]);
+    console.log(carro);
+    $("#resumen").append(`
                       <tr class="fila">
                       <td>${(serviciosJSON[id-1]).id}</td>
                       <td>${(serviciosJSON[id-1].nombre)}</td>
@@ -51,94 +52,93 @@ function agregarAlCarrito(id){
             
                       </tr>`)
 
- 
-addEvent_borrar();
-totalCarro();
 
-//Lo agrego al Local Storage
- localStorage.setItem("Carro",JSON.stringify(carro));
+    addEvent_borrar();
+    totalCarro();
+
+    //Lo agrego al Local Storage
+    localStorage.setItem("Carro", JSON.stringify(carro));
 }
 
 //Borro cada item de la tabla
 
 function addEvent_borrar() {
 
-  let btnDelete = document.querySelectorAll('.delete');
+    let btnDelete = document.querySelectorAll('.delete');
 
-  console.log(btnDelete);
+    console.log(btnDelete);
 
-  btnDelete.forEach(element => {
+    btnDelete.forEach(element => {
 
-      element.addEventListener('click', borraLinea);
+        element.addEventListener('click', borraLinea);
 
-             function borraLinea() {
+        function borraLinea() {
 
-             element.parentNode.parentNode.remove();
-            
-           for(const item of carro){
-            if(
-              element.closest("td").previousElementSibling
-              .previousElementSibling.textContent == item.nombre)
-              {
-              let indice= carro.indexOf(item);
-              carro.splice(indice, 1);
-              console.log(indice);
+            element.parentNode.parentNode.remove();
 
-              
+            for (const item of carro) {
+                if (
+                    element.closest("td").previousElementSibling
+                    .previousElementSibling.textContent == item.nombre) {
+                    let indice = carro.indexOf(item);
+                    carro.splice(indice, 1);
+                    console.log(indice);
 
-              totalCarro();
-              
+
+
+                    totalCarro();
+
+                }
+
             }
-          
-          }
-          
- }
 
- });
+        }
+
+    });
 
 }
-  
 
 
-  
-      
 
-             
 
-             
-          
-          
-           
+
+
+
+
+
+
+
+
 // Cantidad de dinero y servicios contratados
 
 
-function totalCarro(){
- let total = 0;
-  for( const serv of carro){
+function totalCarro() {
+    let total = 0;
+    for (const serv of carro) {
         total += serv.precio;
         console.log(total);
-  }
-        precioUnitario.innerHTML = total;
-        totalServicios.innerHTML = carro.length;
-        
-  }
+    }
+    precioUnitario.innerHTML = total;
+    totalServicios.innerHTML = carro.length;
 
-    
+}
 
 
 
-const tableConteiner = document.getElementById("resumen-total");  
+
+
+const tableConteiner = document.getElementById("resumen-total");
 
 let precioTotal = document.createElement("h2");
 precioTotal.innerHTML = "El total de su compra es $";
 tableConteiner.appendChild(precioTotal);
 
 let precioUnitario = document.createElement("h3");
-precioUnitario.innerHTML =  "0";
+precioUnitario.innerHTML = "0";
 tableConteiner.appendChild(precioUnitario);
 
 let cantidadServicios = document.createElement("h5");
-cantidadServicios.innerHTML ="Total servicios: ";
+cantidadServicios.innerHTML = "Total servicios: ";
 tableConteiner.appendChild(cantidadServicios);
 
 let totalServicios = document.createElement("h2");
@@ -148,21 +148,21 @@ tableConteiner.appendChild(totalServicios);
 
 let reiniciar = document.createElement("button");
 reiniciar.innerHTML = "Recalculando";
-reiniciar.setAttribute("id","reiniciar");
+reiniciar.setAttribute("id", "reiniciar");
 tableConteiner.appendChild(reiniciar);
 
 
 //Boton recalculando
-reiniciar.onclick =()=>{
-  carro = [];
-  precioUnitario.innerHTML= "0";
-  totalServicios.innerHTML= "No ha contratado nada";
-  $('.fila').remove();
-  
+reiniciar.onclick = () => {
+    carro = [];
+    precioUnitario.innerHTML = "0";
+    totalServicios.innerHTML = "No ha contratado nada";
+    $('.fila').remove();
 
-  console.log(carro);
 
-  //localStorage.removeItem("carro");
+    console.log(carro);
+
+    //localStorage.removeItem("carro");
 
 }
 
@@ -172,52 +172,48 @@ reiniciar.onclick =()=>{
 
 //Formulario Footer validacion
 //Funcion llamada desde linea 210 HTML Form
-function validacion(){
- var  name,email,telefono,expresion;
-  name = document.getElementById("name").value;
-  email=document.getElementById("email").value;
-  telefono=document.getElementById("telefono").value;
-  expresion= /\w+@\w+\.+[a-z]/;
+function validacion() {
+    var name, email, telefono, expresion;
+    name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    telefono = document.getElementById("telefono").value;
+    expresion = /\w+@\w+\.+[a-z]/;
 
-  if (name==="" || email ==="" || telefono===""){
-    alert("Todos los campos son obligatorios");
-    return false;
+    if (name === "" || email === "" || telefono === "") {
+        alert("Todos los campos son obligatorios");
+        return false;
 
-  }
-  else if (name.length > 25){
-    alert("Nombre largo");
-    return false;
-  }
-  else if (email.length > 30){
-    alert("Mail incorrecto");
-    return false;
-  } else if(!expresion.test(email)){
-    alert("Mail no cumple");
-    return false;
+    } else if (name.length > 25) {
+        alert("Nombre largo");
+        return false;
+    } else if (email.length > 30) {
+        alert("Mail incorrecto");
+        return false;
+    } else if (!expresion.test(email)) {
+        alert("Mail no cumple");
+        return false;
 
-  }
-  else if (isNaN(telefono)){
-    alert("Telefono invalido");
-    return false;
-  } else if (telefono.length > 20){
-    alert("Telefono largo");
-  }
-  else{
-    
-    const afirmativo=prompt("Genial!!Desea guardar los datos?");
-    var name=name;
-     if(afirmativo ==="si"){
-       localStorage.setItem("nombre",name)
-       localStorage.getItem(name)
-       alert("Hola" + " "+ name+ " "+ "gracias por mirar nuestros servicios!")
-      
-     }else{
-       sessionStorage.removeItem(name)
+    } else if (isNaN(telefono)) {
+        alert("Telefono invalido");
+        return false;
+    } else if (telefono.length > 20) {
+        alert("Telefono largo");
+    } else {
 
-     }
+        const afirmativo = prompt("Genial!!Desea guardar los datos?");
+        var name = name;
+        if (afirmativo === "si") {
+            localStorage.setItem("nombre", name)
+            localStorage.getItem(name)
+            alert("Hola" + " " + name + " " + "gracias por mirar nuestros servicios!")
 
-  }
-  
+        } else {
+            sessionStorage.removeItem(name)
+
+        }
+
+    }
+
 
 }
 
